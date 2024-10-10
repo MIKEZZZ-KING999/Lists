727599989726c20836f11ea7a4d8aee15389d1ba
MIKEZZZ-KING999 <micigwubor@gmail.com>
2024-10-10 17:51:16 +0100

    Add files via upload

---
+++ List.py
@@ -0,0 +1,26 @@
+'''
+names = ["Michael", "Jordan", "Manchester United", "Basketball", "Lamborghini"]
+print(names)
+
+names.append("Catherine")
+print(names)
+
+names.insert(3 , "Jericho")
+print(names)
+'''
+
+fruits = ["Apple", "Banana", "Carrot", "Eggplant", "Lettuce"]
+print(fruits)
+
+fruits.append("Cabbage")
+fruits.append("Watermelon")
+print(fruits)
+
+fruits.insert(4, "Beetroots")
+print(fruits)
+
+print(fruits[3])
+
+print(len(fruits))
+
+print(fruits)
